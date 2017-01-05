ebea91eed3037c8287c323db7d7c49522a4ee77a
Egor Chuchmenkin <biocomando@gmail.com>
2017-01-05 16:01:45 +0700

    Created Lexeme and Cell classes

---
+++ MiniExcel/Cell.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniExcel
+{
+    enum CellType { Expression, Number, Empty, Text }
+    enum Status { OK, ERROR, UNDEFINED}
+
+    class Cell
+    {
+        public CellType Type { get; set; }
+        public Status Status { get; set; }
+        public string Error { get; set; }
+        public int Result { get; set; }
+        public string StringResult { get; set; }
+
+        public Cell()
+        {
+            Status = Status.UNDEFINED;
+        }
+    }
+}
---
+++ MiniExcel/Lexeme.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniExcel
+{
+    enum LexemeType { CellReference, Number, Operation, Text}
+    class Lexeme
+    {
+        public LexemeType Type { get; set; }
+        public string LexemeString { get; set; }
+
+        public Lexeme(LexemeType type, string lexemeString)
+        {
+            Type = type;
+            LexemeString = lexemeString;
+        }
+    }
+}
